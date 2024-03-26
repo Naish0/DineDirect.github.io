771b7f538727ea0f24a8cbc9d69eeca60fd6f47c
Naish0 <165036427+Naish0@users.noreply.github.com>
2024-03-26 06:12:38 -0700

    Added new file

---
+++ script.js
@@ -0,0 +1,18 @@
+function receipt() {
+    var first = "Welcome To Our Shop\nYou purchased: Sample\nPrice: 10.00\nThank You For Buying Our Item!";
+    alert(first);
+}
+
+function add() {
+    var sec = "Welcome To Our Shop\nYou purchased: Sample\nPrice: 15.00\nThank You For Buying Our Item!";
+    alert(sec);
+}
+
+function Receipt() {
+    var third = "Welcome To Our Shop\nYou purchased: Sample\nPrice: 20.00\nThank You For Buying Our Item!";
+    alert(third);
+}
+
+function resibo() {
+    let z = document.getElementById('zero').innerHTML = "0.00";
+}
